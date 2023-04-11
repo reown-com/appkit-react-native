db2d00d3d6bea87487100b8582302140f290222c
ignaciosantise <ignacio.santise@gmail.com>
2023-04-11 11:38:31 -0300

    fix: polyfilling getRandomValues to solve issue with expo 48

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,3 +1,4 @@
+import '../expo-crypto-shim.js';
 import { Alert, StyleSheet, View } from 'react-native';
 import { Web3Modal, Web3Button } from '@web3modal/react-native';
 import { setStringAsync } from 'expo-clipboard';
