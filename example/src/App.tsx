e8a17d06d6c2596d3cfb7156173ec31a1dcba96b
ignaciosantise <ignacio.santise@gmail.com>
2023-04-19 16:46:14 -0300

    fix: removed comment step from action, added env config

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -4,6 +4,7 @@ import '@ethersproject/shims';
 import { Alert, StyleSheet, View } from 'react-native';
 import { Web3Modal, Web3Button } from '@web3modal/react-native';
 import { setStringAsync } from 'expo-clipboard';
+import { Env } from 'example/env.js';
 
 export default function App() {
   const onCopyClipboard = async (value: string) => {
@@ -15,10 +16,7 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Web3Button />
-      <Web3Modal
-        projectId="YOUR_PROJECT_ID"
-        onCopyClipboard={onCopyClipboard}
-      />
+      <Web3Modal projectId={Env.PROJECT_ID} onCopyClipboard={onCopyClipboard} />
     </View>
   );
 }
