d059fbf3820ea59109fb949214f5feed6ca1fef3
ignaciosantise <ignacio.santise@gmail.com>
2023-04-25 17:02:06 -0300

    feat: added isConnected boolean to useWeb3Modal hook

--- src/hooks/useWeb3Modal.ts
+++ src/hooks/useWeb3Modal.ts
@@ -2,14 +2,17 @@ import { useSnapshot } from 'valtio';
 
 import { ModalCtrl } from '../controllers/ModalCtrl';
 import { ClientCtrl } from '../controllers/ClientCtrl';
+import { AccountCtrl } from '../controllers/AccountCtrl';
 
 export function useWeb3Modal() {
   const modalState = useSnapshot(ModalCtrl.state);
+  const accountState = useSnapshot(AccountCtrl.state);
 
   return {
     isOpen: modalState.open,
     open: ModalCtrl.open,
     close: ModalCtrl.close,
     provider: ClientCtrl.state.provider,
+    isConnected: accountState.isConnected,
   };
 }
