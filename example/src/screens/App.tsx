d6c3b7c4c3b3651c4287176d0e1be022341f08e9
ignaciosantise <ignacio.santise@gmail.com>
2023-04-26 12:59:44 -0300

    feat: added personal_sign and eth_sendTransaction buttons

---
+++ example/src/screens/App.tsx
@@ -0,0 +1,83 @@
+import '../../expo-crypto-shim.js';
+import '@walletconnect/react-native-compat';
+import '@ethersproject/shims';
+import { useEffect, useState } from 'react';
+import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Web3Modal, Web3Button, useWeb3Modal } from '@web3modal/react-native';
+import { setStringAsync } from 'expo-clipboard';
+import { ethers } from 'ethers';
+import { Env } from '../../env';
+import { sendTransaction, signMessage } from '../utils/MethodUtil';
+import { sessionParams, providerParams } from '../constants/Config';
+
+export default function App() {
+  const { isConnected, provider } = useWeb3Modal();
+  const [web3Provider, setWeb3Provider] =
+    useState<ethers.providers.Web3Provider>();
+  const onCopyClipboard = async (value: string) => {
+    await setStringAsync(value).then(() => {
+      Alert.alert('Copied', 'Copied to clipboard');
+    });
+  };
+
+  useEffect(() => {
+    if (!web3Provider && isConnected && provider) {
+      setWeb3Provider(new ethers.providers.Web3Provider(provider));
+    }
+  }, [isConnected, provider, web3Provider]);
+
+  return (
+    <View style={styles.container}>
+      <Web3Button />
+      {isConnected && (
+        <View style={styles.buttons}>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => signMessage(web3Provider)}
+          >
+            <Text style={styles.text}>personal_sign</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => sendTransaction(web3Provider)}
+          >
+            <Text style={styles.text}>eth_sendTransaction</Text>
+          </TouchableOpacity>
+        </View>
+      )}
+      <Web3Modal
+        projectId={Env.PROJECT_ID}
+        onCopyClipboard={onCopyClipboard}
+        providerParams={providerParams}
+        sessionParams={sessionParams}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  buttons: {
+    marginTop: 32,
+  },
+  button: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#3396FF',
+    borderRadius: 20,
+    width: 150,
+    height: 50,
+    borderWidth: 1,
+    borderColor: 'rgba(0, 0, 0, 0.1)',
+    marginVertical: 4,
+  },
+  text: {
+    color: 'white',
+    fontWeight: '700',
+  },
+});
---
+++ example/src/utils/MethodUtil.ts
@@ -0,0 +1,53 @@
+import { utf8ToHex } from '@walletconnect/encoding';
+import { ethers } from 'ethers';
+
+export const signMessage = async (
+  web3Provider?: ethers.providers.Web3Provider
+) => {
+  if (!web3Provider) {
+    throw new Error('web3Provider not connected');
+  }
+  const msg = 'Hello World';
+  const hexMsg = utf8ToHex(msg, true);
+  const [address] = await web3Provider.listAccounts();
+  const signature = await web3Provider.send('personal_sign', [hexMsg, address]);
+  return {
+    method: 'personal_sign',
+    address,
+    result: signature,
+  };
+};
+
+export const sendTransaction = async (
+  web3Provider?: ethers.providers.Web3Provider
+) => {
+  if (!web3Provider) {
+    throw new Error('web3Provider not connected');
+  }
+
+  // Get the signer from the UniversalProvider
+  const signer = web3Provider.getSigner();
+
+  const amount = ethers.utils.parseEther('0.001');
+  const address = '0x0000000000000000000000000000000000000000';
+  const transaction = {
+    to: address,
+    value: amount,
+    chainId: 5,
+  };
+
+  // Send the transaction using the signer
+  const txResponse = await signer.sendTransaction(transaction);
+  const transactionHash = txResponse.hash;
+  console.log('transactionHash is ' + transactionHash);
+
+  // Wait for the transaction to be mined (optional)
+  const receipt = await txResponse.wait();
+  console.log('Transaction was mined in block:', receipt.blockNumber);
+
+  return {
+    method: 'eth_sendTransaction',
+    address,
+    transactionHash,
+  };
+};
