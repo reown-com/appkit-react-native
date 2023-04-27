77dfa8290d6169f141e6f1bebc9b8d8341f4c7a6
ignaciosantise <ignacio.santise@gmail.com>
2023-04-27 15:28:41 -0300

    fix: moved common polyfills inside sdk

--- src/index.tsx
+++ src/index.tsx
@@ -1,3 +1,7 @@
+import 'react-native-get-random-values';
+import '@walletconnect/react-native-compat';
+import '@ethersproject/shims';
+
 export { Web3Modal } from './components/Web3Modal';
 export { Web3Button } from './components/Web3Button';
 export { useWeb3Modal } from './hooks/useWeb3Modal';
