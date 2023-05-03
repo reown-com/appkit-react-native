0813ae9b4e885a51ba80398948001cb1eea0c1be
ignaciosantise <ignacio.santise@gmail.com>
2023-05-03 14:29:47 -0300

    refactor: removed unused code from example

--- example/src/screens/App.tsx
+++ example/src/screens/App.tsx
@@ -1,29 +1,19 @@
 import '../../expo-crypto-shim.js';
-import { useEffect, useState } from 'react';
 import { Alert, StyleSheet, View } from 'react-native';
 import { Web3Modal, Web3Button, useWeb3Modal } from '@web3modal/react-native';
 import { setStringAsync } from 'expo-clipboard';
-import { ethers } from 'ethers';
 import { Env } from '../../env';
 import { sessionParams, providerMetadata } from '../constants/Config';
 import { BlockchainActions } from '../components/BlockchainActions';
 
 export default function App() {
-  const { isConnected, provider } = useWeb3Modal();
-  const [web3Provider, setWeb3Provider] =
-    useState<ethers.providers.Web3Provider>();
+  const { isConnected } = useWeb3Modal();
 
   const onCopyClipboard = async (value: string) => {
     await setStringAsync(value).then(() => {
       Alert.alert('Copied', 'Copied to clipboard');
     });
   };
-
-  useEffect(() => {
-    if (!web3Provider && isConnected && provider) {
-      setWeb3Provider(new ethers.providers.Web3Provider(provider));
-    }
-  }, [isConnected, provider, web3Provider]);
 
   return (
     <View style={styles.container}>
