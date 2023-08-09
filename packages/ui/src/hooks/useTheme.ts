c0da4bccc004abd8a277dc87bf48f4fe7f29da4f
ignaciosantise <ignacio.santise@gmail.com>
2023-08-09 17:13:04 -0300

    chore: removed comment

--- packages/ui/src/hooks/useTheme.ts
+++ packages/ui/src/hooks/useTheme.ts
@@ -5,9 +5,6 @@ function useTheme() {
   const scheme = useColorScheme();
   const Theme = scheme === 'dark' ? DarkTheme : LightTheme;
 
-  // How to return customized theme? quizas wrappear el componente con un provider
-  // if (accentColor) return Object.assign(Theme, { accent: accentColor });
-
   return Theme;
 }
 
