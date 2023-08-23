3b18ca92813ea0aeca66388014cd8874167ea823
ignaciosantise <ignacio.santise@gmail.com>
2023-08-23 17:12:05 -0300

    chore: changed babel config to read ui package from source

--- apps/native/babel.config.js
+++ apps/native/babel.config.js
@@ -1,7 +1,22 @@
+const path = require('path');
+const uipak = require('../../packages/ui/package.json');
+
 module.exports = function (api) {
   api.cache(true);
 
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          extensions: ['.tsx', '.ts', '.js', '.json'],
+          alias: {
+            // For development, we want to alias the ui library to the source
+            [uipak.name]: path.join(__dirname, '../../packages/ui', uipak.source)
+          }
+        }
+      ]
+    ]
   };
 };
