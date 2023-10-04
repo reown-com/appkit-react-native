41bb86a437a03e77dc61b82778383734ae34df81
ignaciosantise <ignacio.santise@gmail.com>
2023-10-04 17:06:10 -0300

    chore: changed version of packages

--- packages/viem/src/utils/constants.ts
+++ packages/viem/src/utils/constants.ts
@@ -5,4 +5,4 @@ export const ADD_CHAIN_METHOD = 'wallet_addEthereumChain';
 export const SWITCH_CHAIN_METHOD = 'wallet_switchEthereumChain';
 
 // DO NOT REMOVE, SHOULD MATCH CORE PACKAGE VERSION
-export const VERSION = '0.0.1';
+export const VERSION = '1.0.0-alpha1';
