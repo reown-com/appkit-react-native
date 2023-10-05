73d171e0aeaa5cce8906fcecfa6fa3cc7c7aaf99
ignaciosantise <ignacio.santise@gmail.com>
2023-10-05 15:03:17 -0300

    chore: added clipboard to example

--- apps/native/App.tsx
+++ apps/native/App.tsx
@@ -1,13 +1,56 @@
 import '@walletconnect/react-native-compat';
 import { StyleSheet, View, useColorScheme } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { Web3Modal, W3mButton, createWeb3Modal } from '@web3modal/viem-react-native';
-import { mainnet } from 'viem/chains';
+import * as Clipboard from 'expo-clipboard';
+import {
+  Web3Modal,
+  W3mButton,
+  createWeb3Modal,
+  useProvider,
+  useWeb3ModalState
+} from '@web3modal/viem-react-native';
+import {
+  arbitrum,
+  mainnet,
+  polygon,
+  avalanche,
+  bsc,
+  optimism,
+  gnosis,
+  zkSync,
+  zora,
+  base,
+  celo,
+  aurora
+} from 'viem/chains';
+import { Button } from '@web3modal/ui-react-native';
+import { parseEther } from 'viem';
+
+const chains = [
+  mainnet,
+  polygon,
+  arbitrum,
+  avalanche,
+  bsc,
+  optimism,
+  gnosis,
+  zkSync,
+  zora,
+  base,
+  celo,
+  aurora
+];
+
+const clipboardClient = {
+  setString: async (value: string) => {
+    await Clipboard.setStringAsync(value);
+  }
+};
 
 createWeb3Modal({
   projectId: '90369b5c91c6f7fffe308df2b30f3ace',
-  defaultChain: mainnet,
-  chains: [mainnet],
+  chains,
+  clipboardClient,
   metadata: {
     name: 'Web3Modal v3',
     description: 'Web3Modal v3 by WalletConnect',
@@ -21,11 +64,54 @@ createWeb3Modal({
 
 export default function Native() {
   const isDarkMode = useColorScheme() === 'dark';
+  const { walletClient } = useProvider();
+  const { selectedNetworkId } = useWeb3ModalState();
+
+  const onSignMessage = async () => {
+    const [account] = (await walletClient()?.getAddresses()) ?? [];
+    const signature = await walletClient()?.signMessage({
+      account,
+      message: 'hello world'
+    });
+    console.warn('success', signature);
+  };
+
+  const onSendTransaction = async () => {
+    const [address] = (await walletClient()?.getAddresses()) ?? [];
+    const activeChain = chains.find(chain => chain.id === selectedNetworkId);
+
+    try {
+      const hash = await walletClient()?.sendTransaction({
+        account: address,
+        to: '0xd8dA6BF26964aF9D7eEd9e03E53415D37aA96045', // vitalik.eth
+        value: parseEther('0.001'),
+        data: '0x',
+        chain: activeChain
+      });
+
+      return {
+        method: 'send transaction',
+        response: hash
+      };
+    } catch {}
+  };
 
   return (
     <View style={[styles.container, isDarkMode && styles.dark]}>
       <StatusBar style="auto" />
-      <W3mButton label="Connect" loadingLabel="Connecting..." balance="show" />
+      <W3mButton
+        connectStyle={styles.button}
+        accountStyle={styles.button}
+        label="Connect"
+        loadingLabel="Connecting..."
+        balance="show"
+      />
+      <Button style={styles.button} onPress={() => onSignMessage()}>
+        Sign
+      </Button>
+      <Button style={styles.button} onPress={() => onSendTransaction()}>
+        Send
+      </Button>
       <Web3Modal />
     </View>
   );
@@ -43,5 +129,8 @@ const styles = StyleSheet.create({
   },
   text: {
     marginBottom: 20
+  },
+  button: {
+    marginVertical: 6
   }
 });
--- apps/native/babel.config.js
+++ apps/native/babel.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const uipak = require('../../packages/ui/package.json');
 const corepak = require('../../packages/core/package.json');
 const scaffoldpak = require('../../packages/scaffold/package.json');
+const viempak = require('../../packages/viem/package.json');
 
 module.exports = function (api) {
   api.cache(true);
@@ -17,7 +18,8 @@ module.exports = function (api) {
             // For development, we want to alias the ui library to the source
             [uipak.name]: path.join(__dirname, '../../packages/ui', uipak.source),
             [corepak.name]: path.join(__dirname, '../../packages/core', corepak.source),
-            [scaffoldpak.name]: path.join(__dirname, '../../packages/scaffold', scaffoldpak.source)
+            [scaffoldpak.name]: path.join(__dirname, '../../packages/scaffold', scaffoldpak.source),
+            [viempak.name]: path.join(__dirname, '../../packages/viem', viempak.source)
           }
         }
       ]
