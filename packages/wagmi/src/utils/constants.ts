838fe0bfdd2c81ce4226e66cecdcd921e267ade7
ignaciosantise <ignacio.santise@gmail.com>
2023-11-29 16:28:28 -0300

    fix: changed version constant + added script to auto-update when build

--- packages/wagmi/src/utils/constants.ts
+++ packages/wagmi/src/utils/constants.ts
@@ -13,4 +13,4 @@ export const ADD_CHAIN_METHOD = 'wallet_addEthereumChain';
 export const WALLET_CHOICE_KEY = 'wagmi.wallet';
 
 // DO NOT REMOVE, SHOULD MATCH @web3modal/core-react-native VERSION
-export const VERSION = '1.0.0-beta.3';
+export const VERSION = '1.0.0';
