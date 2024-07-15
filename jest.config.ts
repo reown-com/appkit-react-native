700900a7d7078787c22ca00541b4ccc0728dc928
ignaciosantise <25931366+ignaciosantise@users.noreply.github.com>
2024-07-15 14:00:23 -0300

    chore: removed qodana old code

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,5 @@
 module.exports = {
   preset: 'react-native',
   modulePathIgnorePatterns: ['<rootDir>/node_modules', '<rootDir>/packages/*/lib/'],
-  coverageDirectory: '.qodana/code-coverage/',
   coverageReporters: ['lcovonly']
 };
