4ba59e62fd7f80a1fe6a064fedb07d5126da211a
ignaciosantise <25931366+ignaciosantise@users.noreply.github.com>
2024-10-15 17:27:40 -0300

    chore: wrapped discover view in bottomsheet view

--- packages/scaffold/src/views/w3m-upgrade-to-smart-account-view/index.tsx
+++ packages/scaffold/src/views/w3m-upgrade-to-smart-account-view/index.tsx
@@ -1,3 +1,5 @@
+import { Linking } from 'react-native';
+import { BottomSheetView } from '@gorhom/bottom-sheet';
 import { useState } from 'react';
 import { Button, FlexView, Icon, Link, Text, Visual } from '@reown/appkit-ui-react-native';
 import {
@@ -9,7 +11,6 @@ import {
   SnackController
 } from '@reown/appkit-core-react-native';
 import styles from './styles';
-import { Linking } from 'react-native';
 
 export function UpgradeToSmartAccountView() {
   const [isAccountTypeLoading, setIsAccountTypeLoading] = useState(false);
@@ -42,39 +43,41 @@ export function UpgradeToSmartAccountView() {
   return (
     <>
       <Icon name="close" onPress={RouterController.goBack} style={styles.closeButton} />
-      <FlexView style={styles.container} padding={['4xl', 'm', '2xl', 'm']}>
-        <FlexView alignItems="center" justifyContent="center" flexDirection="row">
-          <Visual name="google" />
-          <Visual style={styles.middleIcon} name="pencil" />
-          <Visual name="lightbulb" />
+      <BottomSheetView>
+        <FlexView style={styles.container} padding={['4xl', 'm', '2xl', 'm']}>
+          <FlexView alignItems="center" justifyContent="center" flexDirection="row">
+            <Visual name="google" />
+            <Visual style={styles.middleIcon} name="pencil" />
+            <Visual name="lightbulb" />
+          </FlexView>
+          <Text variant="medium-600" color="fg-100" style={styles.title}>
+            Discover Smart Accounts
+          </Text>
+          <Text variant="paragraph-400" color="fg-100" center>
+            Access advanced brand new features as username, improved security and a smoother user
+            experience!
+          </Text>
+          <FlexView flexDirection="row" margin={['m', '4xl', 'm', '4xl']}>
+            <Button
+              variant="accent"
+              onPress={RouterController.goBack}
+              style={[styles.button, styles.cancelButton]}
+            >
+              Do it later
+            </Button>
+            <Button
+              onPress={onSwitchAccountType}
+              loading={isAccountTypeLoading}
+              style={styles.button}
+            >
+              Continue
+            </Button>
+          </FlexView>
+          <Link onPress={onLearnMorePress} iconRight="externalLink">
+            Learn more
+          </Link>
         </FlexView>
-        <Text variant="medium-600" color="fg-100" style={styles.title}>
-          Discover Smart Accounts
-        </Text>
-        <Text variant="paragraph-400" color="fg-100" center>
-          Access advanced brand new features as username, improved security and a smoother user
-          experience!
-        </Text>
-        <FlexView flexDirection="row" margin={['m', '4xl', 'm', '4xl']}>
-          <Button
-            variant="accent"
-            onPress={RouterController.goBack}
-            style={[styles.button, styles.cancelButton]}
-          >
-            Do it later
-          </Button>
-          <Button
-            onPress={onSwitchAccountType}
-            loading={isAccountTypeLoading}
-            style={styles.button}
-          >
-            Continue
-          </Button>
-        </FlexView>
-        <Link onPress={onLearnMorePress} iconRight="externalLink">
-          Learn more
-        </Link>
-      </FlexView>
+      </BottomSheetView>
     </>
   );
 }
