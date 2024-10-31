ffb46806138f3538fe1b10c3cea3adcb77a41152
ignaciosantise <25931366+ignaciosantise@users.noreply.github.com>
2024-10-31 16:46:34 -0300

    chore: fixed text test

--- packages/ui/src/__tests__/wui-text.test.tsx
+++ packages/ui/src/__tests__/wui-text.test.tsx
@@ -1,5 +1,14 @@
 import { render } from '@testing-library/react-native';
+import { configureInternal } from '@testing-library/react-native/build/config';
 import { Text } from '../components/wui-text';
+
+configureInternal({
+  hostComponentNames: {
+    text: 'RCTText',
+    textInput: 'RCTTextInput',
+    switch: 'RCTSwitch'
+  }
+});
 
 test('Text render', () => {
   const label = 'Hello World';
