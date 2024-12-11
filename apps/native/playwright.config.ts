f80555b2f352d515b19a9413bf8ae87a4366255a
ignaciosantise <25931366+ignaciosantise@users.noreply.github.com>
2024-12-11 18:09:20 -0300

    chore: action change

--- apps/native/playwright.config.ts
+++ apps/native/playwright.config.ts
@@ -32,20 +32,15 @@ export default defineConfig({
     trace: 'on-first-retry',
     permissions: ['clipboard-read', 'clipboard-write'],
     navigationTimeout: 30000,
-    actionTimeout: 30000
+    actionTimeout: 30000,
+    headless: !!process.env.CI
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
       name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome'],
-        launchOptions: {
-          headless: true,
-          args: ['--no-sandbox', '--disable-setuid-sandbox']
-        }
-      }
+      use: { ...devices['Desktop Chrome'] }
     }
   ],
 
