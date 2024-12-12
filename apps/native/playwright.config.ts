daa69220a7e3d5224528c2d2b5e4fa92092848ad
ignaciosantise <25931366+ignaciosantise@users.noreply.github.com>
2024-12-12 11:52:31 -0300

    chore: action change

--- apps/native/playwright.config.ts
+++ apps/native/playwright.config.ts
@@ -13,27 +13,29 @@ import { defineConfig, devices } from '@playwright/test';
  */
 export default defineConfig({
   testDir: './tests',
-  /* Run tests in files in parallel */
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
+
   /* Retry on CI only */
   retries: process.env.CI ? 2 : 0,
+
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
+
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [['html'], ['list']],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
+
   use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: process.env.BASE_URL || 'http://localhost:8081',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+
     permissions: ['clipboard-read', 'clipboard-write'],
     navigationTimeout: 30000,
     actionTimeout: 30000,
-    headless: !!process.env.CI
+    video: 'retain-on-failure'
   },
 
   /* Configure projects for major browsers */
@@ -45,14 +47,14 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npx serve dist --single',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  //   timeout: 30000,
-  //   stdout: 'pipe',
-  //   stderr: 'pipe'
-  // },
+  webServer: {
+    command: 'yarn web',
+    url: 'http://localhost:8081',
+    reuseExistingServer: !process.env.CI,
+    timeout: 30000,
+    stdout: 'pipe',
+    stderr: 'pipe'
+  },
 
   globalTimeout: 600000,
   expect: {
