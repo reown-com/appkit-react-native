3d06ddffecd542e9b20dd13046a0323939622cb1
nacho <25931366+ignaciosantise@users.noreply.github.com>
2025-05-08 12:08:25 -0300

    chore: removed scaffold package

--- packages/scaffold/src/partials/w3m-header/styles.ts
+++
@@ -1,8 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export default StyleSheet.create({
-  iconPlaceholder: {
-    height: 32,
-    width: 32
-  }
-});
--- packages/scaffold/src/views/w3m-wallet-compatible-networks-view/styles.ts
+++
@@ -1,8 +0,0 @@
-import { Spacing } from '@reown/appkit-ui-react-native';
-import { StyleSheet } from 'react-native';
-
-export default StyleSheet.create({
-  image: {
-    marginRight: Spacing.s
-  }
-});
