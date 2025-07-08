235b20472b0f8a6b0d4f3417a3a4476d23bcc24f
nacho <25931366+ignaciosantise@users.noreply.github.com>
2025-07-08 12:55:54 -0300

    chore: removed auth packages

--- packages/wallet/src/index.ts
+++
@@ -1,7 +0,0 @@
-export { AppKitFrameHelpers } from './AppKitFrameHelpers';
-export { AppKitFrameProvider } from './AppKitFrameProvider';
-export { AppKitFrameSchema } from './AppKitFrameSchema';
-export { AppKitFrameConstants, AppKitFrameRpcConstants } from './AppKitFrameConstants';
-export { AppKitFrameStorage } from './AppKitFrameStorage';
-export { AuthWebview } from './AppKitAuthWebview';
-export type { AppKitFrameTypes } from './AppKitFrameTypes';
