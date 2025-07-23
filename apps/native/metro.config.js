a69f47627ddac159c4b9a270168ea50a70b0844a
dmcclung <35938+dmcclung@users.noreply.github.com>
2025-07-23 14:40:03 -0500

    fix: force native entry point for WalletConnect ethereum provider and update builder-bob

--- apps/native/metro.config.js
+++ apps/native/metro.config.js
@@ -18,4 +18,20 @@ config.resolver.nodeModulesPaths = [
 // 3. Force Metro to resolve (sub)dependencies only from the `nodeModulesPaths`
 config.resolver.disableHierarchicalLookup = true;
 
+// 4. Force react native entry point for web platform
+config.resolver.resolveRequest = (context, moduleName, platform) => {
+  if (moduleName === '@walletconnect/ethereum-provider') {
+    const nativePath = path.resolve(
+      workspaceRoot,
+      'node_modules/@walletconnect/ethereum-provider/dist/index.native.js'
+    );
+    
+    return {
+      type: 'sourceFile',
+      filePath: nativePath,
+    };
+  }
+  return context.resolveRequest(context, moduleName, platform);
+};
+
 module.exports = config;
